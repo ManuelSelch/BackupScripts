8e625becd1625be126962c5b77462dcf985e9337
Manuel Selch <manuel@selch.de>
2025-01-26 13:01:06 +0100

    install script

---
+++ borgmatic/install.sh
@@ -0,0 +1,9 @@
+# installation
+sudo apt update
+sudo apt install borgbackup
+
+sudo apt install python3-pip
+sudo pip3 install borgmatic
+
+# maria-dump for database backups
+sudo apt install mariadb-client
